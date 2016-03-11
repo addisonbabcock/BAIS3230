15e647e4046f99f709faec73ee3e37bc39b5d502
Addison Babcock <addison.babcock@hotmail.com>
2016-03-11 09:06:16 -0700

    Added some defaults.

--- GolfCourseManager/GolfCourseManager/Models/Member.cs
+++ GolfCourseManager/GolfCourseManager/Models/Member.cs
@@ -13,13 +13,14 @@ namespace GolfCourseManager.Models
 		public int GolfCourseId { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
+		public string Email { get; set; }
 
 		public enum MemberStatus { Active, Unpaid, Applied, Inactive, Closed }
-		public MemberStatus Status { get; set; }
+		public MemberStatus Status { get; set; } = MemberStatus.Applied;
 
 		public string Address1 { get; set; }
-		public string Address2 { get; set; }
-		public string Address3 { get; set; }
+		public string Address2 { get; set; } = String.Empty;
+		public string Address3 { get; set; } = String.Empty;
 		public string City { get; set; }
 		public string Province { get; set; }
 		public string PostalCode { get; set; }
